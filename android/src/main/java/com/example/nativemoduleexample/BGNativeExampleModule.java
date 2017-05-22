5da34ef87b700df76b450cfe9cec4b96202b1a55
zhakang <956468883@qq.com>
2017-05-22 19:24:43 +0800

    修改

--- android/src/main/java/com/example/nativemoduleexample/BGNativeExampleModule.java
+++ android/src/main/java/com/example/nativemoduleexample/BGNativeExampleModule.java
@@ -2,10 +2,13 @@ package com.example.nativemoduleexample;
 
 import android.util.Log;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 
 import static com.facebook.react.common.ReactConstants.TAG;
 
@@ -23,8 +26,9 @@ public class BGNativeExampleModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void testPrint(String name, ReadableMap info) {
-        Log.i(TAG, name);
-        Log.i(TAG, info.toString());
+    public void testPrint(String name, Promise promise) {
+        WritableMap map = Arguments.createMap();
+        map.putString("name", name);
+        promise.resolve(map);
     }
 }
